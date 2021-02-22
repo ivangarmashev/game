f490318fda89d25b43f75cf737f09bc8987eb6c8
ivang <ivan.garmashev>
2021-02-22 19:44:29 +0300

    123

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 import pygame
 import random
 
-WIDTH = 360  # ширина игрового окна
-HEIGHT = 480 # высота игрового окна
-FPS = 60 # частота кадров в секунду
+WIDTH = 512  # ширина игрового окна
+HEIGHT = 512  # высота игрового окна
+FPS = 130  # частота кадров в секунду
 # Цвета (R, G, B)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -16,19 +16,22 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 pygame.init()
-pygame.mixer.init()  # для звука
+# pygame.mixer.init()  # для звука
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("123")
 clock = pygame.time.Clock()
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x=None):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 50))
+        self.image = pygame.Surface((2, 5))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        if x:
+            self.rect.center = (12, 32)
+        else:
+            self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.speedx = 0
         self.speedy = 0
 
@@ -44,13 +47,13 @@ class Player(pygame.sprite.Sprite):
         self.speedy = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.speedx = -8
+            self.speedx = -1
         if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
+            self.speedx = 1
         if keystate[pygame.K_UP]:
-            self.speedy = -8
+            self.speedy = -1
         if keystate[pygame.K_DOWN]:
-            self.speedy = 8
+            self.speedy = 1
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.right > WIDTH:
@@ -61,9 +64,9 @@ class Player(pygame.sprite.Sprite):
 
 all_sprites = pygame.sprite.Group()
 player = Player()
-all_sprites.add(player)
+player2 = Player('123')
+all_sprites.add(player, player2)
 running = True
-
 
 while running:
     # Держим цикл на правильной скорости
@@ -79,8 +82,10 @@ while running:
     # Ввод процесса (события)
     # Обновление
     # Визуализация (сборка)
+    ev = pygame.event.get()
+    print(ev)
+    for event in ev:
 
-    for event in pygame.event.get():
         # проверить закрытие окна
         if event.type == pygame.QUIT:
             running = False
@@ -92,6 +97,5 @@ while running:
         #     player.rect.x += 5
         # if event.type == pygame.K_RIGHT:
         #     player.rect.x -= 5
-
 
 pygame.quit()
